86319e86ca1d49d7fe0e71f18f6887312f7d413b
silkie-chicken5 <113161134+silkie-chicken5@users.noreply.github.com>
2024-02-15 21:39:46 -0500

    test: BroadbandHandler

---
+++ CSV/MockedACSCensusSource.java
@@ -0,0 +1,19 @@
+package CSV;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class MockedACSCensusSource implements CensusDatasource{
+
+  private final CensusData constantData;
+
+  public MockedACSCensusSource(CensusData constantData) {
+    this.constantData = constantData;
+  }
+
+  @Override
+  public CensusData getCensusData(String stateName, String countyName)
+      throws DatasourceException, IllegalArgumentException, URISyntaxException, IOException, InterruptedException {
+    return this.constantData;
+  }
+}
---
+++ CSV/TestACSCensusSource.java
@@ -0,0 +1,6 @@
+package CSV;
+
+public class TestACSCensusSource {
+
+
+}
