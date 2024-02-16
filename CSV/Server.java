3f6551777595e4ae951a474b65f7f0374df6c7ae
zhangk3714 <kenneth.c.zhang@gmail.com>
2024-02-15 21:14:25 -0500

    refactor: merge servers

--- CSV/Server.java
+++ CSV/Server.java
@@ -4,6 +4,12 @@ import static spark.Spark.after;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+
+import CSV.LoadSearchView.DataSource;
+import CSV.LoadSearchView.LoadHandler;
+import CSV.LoadSearchView.SearchHandler;
+import CSV.LoadSearchView.ViewHandler;
+import CSV.Parser.CSVSearch;
 import spark.Spark;
 
 public class Server {
@@ -20,6 +26,10 @@ public class Server {
       response.header("Access-Control-Allow-Methods", "GET"); // ask for only GET?
     });
 
+    DataSource<CSVSearch> dataSource = new DataSource<>();
+    Spark.get("loadcsv", new LoadHandler(dataSource));
+    Spark.get("viewcsv", new ViewHandler(dataSource));
+    Spark.get("searchcsv", new SearchHandler(dataSource));
     Spark.get("broadband", new BroadbandHandler(new ACSCensusSource()));
     Spark.init();
     Spark.awaitInitialization();
